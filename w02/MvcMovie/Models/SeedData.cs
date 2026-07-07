d28a678090bbc8af1781fa6350cec7c390c46777
bitfws <bitfws.xai@gmail.com>
2026-07-06 22:25:21 -0500

    W02 Assignment: ASP.NET Core

---
+++ w02/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,92 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // If the movie already exists, it is not inserted again.
+            if (context.Movie.Any())
+            {
+                return;
+            }
+
+
+            context.Movie.AddRange(
+
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Rating = "R",
+                    Price = 7.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Rating = "R",
+                    Price = 8.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Rating = "PG",
+                    Price = 9.99M
+                },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Rating = "G",
+                    Price = 3.99M
+                },
+
+                new Movie
+                {
+                    Title = "Interstellar",
+                    ReleaseDate = DateTime.Parse("2014-11-07"),
+                    Genre = "Science Fiction",
+                    Rating = "PG-13",
+                    Price = 12.99M
+                },
+
+                new Movie
+                {
+                    Title = "The Dark Knight",
+                    ReleaseDate = DateTime.Parse("2008-07-18"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Price = 10.99M
+                },
+
+                new Movie
+                {
+                    Title = "The Matrix",
+                    ReleaseDate = DateTime.Parse("1999-03-31"),
+                    Genre = "Science Fiction",
+                    Rating = "R",
+                    Price = 9.99M
+                }
+
+            );
+
+
+            context.SaveChanges();
+        }
+    }
+}
